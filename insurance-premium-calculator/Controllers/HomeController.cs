bed628a4428a906d6ac9463b856f573e6a593fc9
Daire Finn <daire.finn@mail.itsligo.ie>
2019-02-19 17:36:17 +0000

    Form hooked up

--- insurance-premium-calculator/Controllers/HomeController.cs
+++ insurance-premium-calculator/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using insurance_premium_calculator.ViewModels;
+using insurance_premium_calculator.lib;
 
 namespace insurance_premium_calculator.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(InsuranceViewModel model)
         {
+            InsuranceService service = new InsuranceService();
+
+            ViewBag.age = model.age;
+            ViewBag.gender = model.gender;
+            ViewBag.premium = service.CalcPremium(model.age, model.gender);
+
             return View();
         }
     }
---
+++ insurance-premium-calculator/ViewModels/InsuranceViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace insurance_premium_calculator.ViewModels
+{
+    public class InsuranceViewModel
+    {
+        [Required]
+        [Display(Name = "Age")]
+        public int age { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string gender { get; set; }
+
+        [Display(Name = "Premium")]
+        public double premium { get; set; }
+    }
+}
