7be1cf633afa4f76dca280086a051886daa5670e
Killian Kelly <Killiank360@gmail.com>
2019-02-19 22:11:56 +0000

    Added fitnesse fixture code and table

---
+++ insurance-premium-calculator.Tests/CalcPremiumFixture.cs
@@ -0,0 +1,14 @@
+﻿using fit;
+using insurance_premium_calculator.lib;
+
+namespace insurance_premium_calculator.Tests
+{
+    public class CalcPremiumFixture : ColumnFixture
+    {
+        public int age;
+        public string gender;
+        public InsuranceService testObj = new InsuranceService();
+
+        public double premium() => testObj.CalcPremium(age, gender);
+    }
+}
